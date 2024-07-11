7a1a3d7ead7bcd7b7c9891d171292d6854da5f94
DreamCutter28 <martirosovsergey777@gmail.com>
2024-07-12 04:19:09 +0800

    requirements fix + small changes
    удалено много ненужных зависимостей;
    таблицы в базе данных теперь создаются при запуске, если их нет

--- backend/database.py
+++ backend/database.py
@@ -8,6 +8,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    
 def get_db():
     db = SessionLocal()
     try:
